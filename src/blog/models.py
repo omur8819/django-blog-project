b37869a4af208c240460c92a7acfbf79b54a71fb
omur8819 <omur8819@gmail.com>
2021-01-03 02:20:45 +0300

    Crated Category model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,7 +1,9 @@
 from typing import ContextManager
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
 class Post(models.Model):
     OPTIONS = (
@@ -11,9 +13,9 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField()
-    # category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
