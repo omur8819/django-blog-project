f2a692f5b2e84691ee03334b11775d7d0f821b12
omur8819 <omur8819@gmail.com>
2021-01-03 02:12:42 +0300

    Created Post model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,19 @@
+from typing import ContextManager
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    OPTIONS = (
+        ('d', 'Draft'),
+        ('p', 'Published')
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField()
+    # category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=OPTIONS, default='d')
+    slug = models.SlugField(blank=True, unique=True)
