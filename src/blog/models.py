a7f62121ebaaf281a0a1a0aaf085709fbc0e76f8
omur8819 <omur8819@gmail.com>
2021-01-09 14:08:05 +0300

    Project updated

--- src/blog/models.py
+++ src/blog/models.py
@@ -2,8 +2,19 @@ from typing import ContextManager
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    return 'blog/{0}/{1}'.format(instance.author.id, filename)
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+        
+    def __str__(self):
+        return self.name
+    
+    
 
 class Post(models.Model):
     OPTIONS = (
@@ -12,10 +23,15 @@ class Post(models.Model):
     )
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to=user_directory_path, default='django.jpg')
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='d')
     slug = models.SlugField(blank=True, unique=True)
+    
+    def __str__(self):
+        return self.title
+    
+   
